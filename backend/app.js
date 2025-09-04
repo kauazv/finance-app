0691fc72accc0103783d3228718e51e126c3529f
Kauã Ferrari <kaua.ferrari04@icloud.com>
2025-09-03 23:43:15 -0300

    criando modelo usuario para o mongodb

--- backend/app.js
+++ backend/app.js
@@ -9,3 +9,7 @@ app.use(express.json(), cors());
 moongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/finance-app', {
   useNewUrlParser: true,
 });
+
+const User = require('./models/User');
+const Transaction = require('./models/Transaction');
+const authMiddleware = require('./utils/auth');
