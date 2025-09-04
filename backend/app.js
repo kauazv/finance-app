dd5f328df3f1467fb89de976bd42b8694e2c65c1
Kauã Ferrari <kaua.ferrari04@icloud.com>
2025-09-03 23:35:55 -0300

    criando uma api com express para receber dados json e conectando ao mongodb local

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json(), cors());
+
+moongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/finance-app', {
+  useNewUrlParser: true,
+});
