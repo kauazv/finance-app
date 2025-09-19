0ee5239f2c81a6fbf32dec3094c2755904ae00ab
Kauã Ferrari <kaua.ferrari04@icloud.com>
2025-09-18 23:33:15 -0300

    função estava digitada errada, consertando cagada e agora misteriosamente esta reconhecendo :p

--- backend/app/api/transactions.py
+++ backend/app/api/transactions.py
@@ -6,7 +6,7 @@ from app.database import get_db
 from app.auth import get_current_user
 from datetime import datetime
 
-router = APIROUTER()
+router = APIRouter()
 
 def add_transaction(data: TransactionCreate, db: Session = Depends(get_db), user_id=Depends(get_current_user)):
     trx = Transaction(**data.dict(), user_id=user_id, date=datetime.utcnow())
